c524372c46c634871c839577216e44b9c5406c0f
shamshadhusain3 <husainshamshad68.com>
2023-09-14 14:54:12 +0530

    first commit

---
+++ resources/wms/footer.blade.php
@@ -0,0 +1,61 @@
+<footer>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4">
+                    <div class="col-one">
+                        <h4><span class="orange">food</span>saver foundation</h4>
+                        <p>Address : NSTI Kanpur ,Govind Nagar CTI.
+                        <p>Contact No : <a href="tel: +91 8604991151,6383814585">+91 8604991151,6383814585</a></p>
+                        <p>Email : <a href="mailto:husainshamshad68@gmail.com">husainshamshad68@gmail.com</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="col-two">
+                        <h4>Important Links</h4>
+                        <ul>
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#donation">Donations</a></li>
+                            <li><a href="#mission-id">Missions</a></li>
+                            <li><a href="#about">About us</a></li>
+                            <li><a href="#contact">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="col-one">
+                        <h4>Social Media</h4>
+                        <div class="social">
+                            <a href="https://www.facebook.com/sidhu.bhaiji.9?mibextid=ZbWKwL"><img
+                                    src="img/icons/facebook.png" alt="facebook"></a>
+                            <a href="#"><img src="img/icons/instagram.png" alt="inatagram"></a>
+                            <a href="https://instagram.com/_.shamshad._9?igshid=MzNlNGNkZWQ4Mg=="><img
+                                    src="img/icons/youtube.png" alt="youtube"></a>
+                            <a href="https://www.linkedin.com/in/shamshad-husain-538212252/"><img
+                                    src="img/icons/linkedin.png " alt="linkedin"></a>
+                            <a href="mailto:asthasachan96@gmail.com"><img src="img/icons/gmail.png" alt="gnail"></a>
+                        </div>
+                        <p>Copyright &copy; 2022 | All Right Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/script.js"></script>
+    <!-- <script>
+        document.querySelectorAll('.image-container img').forEach(image => {
+    image.onclick = () => {
+        document.querySelector('.pop-image').style.display = 'block';
+        document.querySelector('.pop-image img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.pop-image span').onclick = () => {
+    document.querySelector('.pop-image').style.display = 'none';
+}
+    </script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
